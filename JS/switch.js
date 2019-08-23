9881578233dd110fb87cc56bbd7cfc9e504eff7b
Raj <raj.fps2000@gmail.com>
2019-08-23 22:40:57 +0530

    shit i got the to material morf

--- JS/switch.js
+++ JS/switch.js
@@ -70,21 +70,17 @@ const changeStyleSheet = styleSheet => {
     /* document.querySelector('.trans').media=''; */
 
 };
-const animTo=(t,cover)=>{
-    t.fromTo(cover, .5, { width: '0%' }, { width: '100%', ease: Power2.easeInOut }).fromTo(cover, .5, { width: '100%' }, { width: '0%', ease: Power2.easeInOut },'+=.05');
-}
+
 const reloadStyle = () => {
     var Style = styles.reduce((newest, style) => {
         return (newest.state || 0) > style.state ? newest : style;
     }, {});
-    const t = new TimelineMax();
-    var cover = document.querySelector('.main-cover');
     if (Style.styleName === 'flat') {
         if (Style.styleName != previousStyleSheet.id) {
-            animTo(t,cover);
-            setTimeout(()=>{
+            
+            
                 changeStyleSheet(Style.styleName);
-            }, 550);
+            
         }
         if (Style.theme === 'dark') {
             setCSSvar('bgtheme', Style.dark.bgtheme);
@@ -104,10 +100,9 @@ const reloadStyle = () => {
         }
     } else if (Style.styleName === 'material') {
         if (Style.styleName != previousStyleSheet.id) {
-            animTo(t, cover);
-            setTimeout(()=>{
+            
                 changeStyleSheet(Style.styleName);
-            }, 550);
+            
         }
 
         if (Style.theme === 'dark') {
