ee39eab24823e364deb21edc7104e35d21ad414a
Raj <raj.fps2000@gmail.com>
2019-08-24 12:15:42 +0530

    remove useless code

--- JS/switch.js
+++ JS/switch.js
@@ -13,13 +13,13 @@ var styles = [
     {
         styleName: 'flat',
         theme: 'dark',
-        dark: {
+        'dark': {
             bgtheme: 'black',
             textColor: 'white',
             buttonbg: 'white',
             buttontext: 'black',
         },
-        light: {
+        'light': {
             bgtheme: 'white',
             textColor: 'black',
             buttonbg: 'black',
@@ -58,23 +58,33 @@ const reloadStyle = () => {
     var Style = styles.reduce((newest, style) => {
         return (newest.state || 0) > style.state ? newest : style;
     }, {});
+    const setFlat = (t) => {
+        console.log(Style[t]);
+        setCSSvar('bgtheme', Style[t].bgtheme);
+        setCSSvar('textColor', Style[t].textColor);
+        setCSSvar('buttonbg', Style[t].buttonbg);
+        setCSSvar('buttontext', Style[t].buttontext);
+    }
+    const setMaterial=(t)=>{
+        setCSSvar('bgtheme', Style[t].bgtheme);
+        setCSSvar('lightbgtheme', Style[t].lightbgtheme)
+        setCSSvar('buttonbg', Style[t].buttonbg);
+        setCSSvar('textColor', Style[t].textColor);
+        setCSSvar('buttontext', Style[t].buttontext);
+    }
+
+
     if (Style.styleName === 'flat') {
         if (Style.styleName != previousStyleSheet) {
             changeStyleSheet(Style.styleName);
         }
         if (Style.theme === 'dark') {
-            setCSSvar('bgtheme', Style.dark.bgtheme);
-            setCSSvar('textColor', Style.dark.textColor);
-            setCSSvar('buttonbg', Style.dark.buttonbg);
-            setCSSvar('buttontext', Style.dark.buttontext);
+            setFlat(Style.theme);
             setCSSvar('flatdarkbutton', '2%');
             setCSSvar('flatlightbutton', '1%');
         }
         else if (Style.theme === 'light') {
-            setCSSvar('bgtheme', Style.light.bgtheme);
-            setCSSvar('textColor', Style.light.textColor);
-            setCSSvar('buttonbg', Style.light.buttonbg);
-            setCSSvar('buttontext', Style.light.buttontext);
+            setFlat(Style.theme);
             setCSSvar('flatdarkbutton', '1%');
             setCSSvar('flatlightbutton', '2%');
         }
@@ -84,27 +94,16 @@ const reloadStyle = () => {
         }
 
         if (Style.theme === 'dark') {
-            setCSSvar('bgtheme', Style.dark.bgtheme);
-            setCSSvar('lightbgtheme', Style.dark.lightbgtheme)
-            setCSSvar('textColor', Style.dark.textColor);
-            setCSSvar('buttonbg', Style.dark.buttonbg);
-            setCSSvar('buttontext', Style.dark.buttontext);
+            setMaterial(Style.theme);
             setCSSvar('materialdarkbutton', '2%');
             setCSSvar('materiallightbutton', '1%');
         }
         else if (Style.theme === 'light') {
-            setCSSvar('bgtheme', Style.light.bgtheme);
-            setCSSvar('textColor', Style.light.textColor);
-            setCSSvar('buttonbg', Style.light.buttonbg);
-            setCSSvar('buttontext', Style.light.buttontext);
+            setMaterial(Style.theme);
             setCSSvar('materialdarkbutton', '1%');
             setCSSvar('materiallightbutton', '2%');
         }
     }
-    /* getComputedStyle(document.documentElement)
-        .getPropertyValue('--my-variable-name'); */
-
-
 };
 const setCSSvar = (property, value) => {
     property = '--' + property;
