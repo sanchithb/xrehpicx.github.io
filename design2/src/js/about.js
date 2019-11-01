e9c2374b127ee5652e1fc0b0bb4353addaaa7b7c
Raj <raj.fps2000@gmail.com>
2019-11-01 22:42:31 +0530

    updated photos

--- design2/src/js/about.js
+++ design2/src/js/about.js
@@ -3,7 +3,7 @@ const test = () => {
     const slider = document.querySelector('.slider');
     t.fromTo(slider, 1, { top: '0px', opacity: '0' }, { top: '0px', opacity: '1' }, '+=1.5')
         .fromTo('.filter', 1, { top: '0px', opacity: '0' }, { top: '0px', opacity: '1', onComplete: show });
-    for (var i = 1; i < 5; i++) {
+    for (var i = 1; i < 7; i++) {
         const imageElement = document.createElement('img');
         const image = document.createElement('div');
         image.className = 'bg-image';
@@ -12,12 +12,13 @@ const test = () => {
         image.appendChild(imageElement);
     }
     slider.style.transition = 'transform 1s cubic-bezier(1,0,0,1)';
-    const positions = [-30, -200, -420, -700];
+    const positions = [-30, -200, -420, -680, -910, -1155];
     let counter = 1;
+    //slider.style.transform = 'translateX(-1155vw)';
     setInterval(() => {
         slider.style.transform = 'translateX(' + positions[counter] + 'vw)';
         console.log('set');
-        counter = counter !== 3 ? counter += 1 : counter = 0;
+        counter = counter !== 5 ? counter += 1 : counter = 0;
     }, 3000);
 }
 test();
@@ -33,14 +34,18 @@ function show() {
         .to(img, time, { opacity: '1' })
         .to(img, time, { opacity: '0', onComplete: () => img.src = './assets/img/arr2/img7.jpg' }, '+=1')
         .to(img, time, { opacity: '1' })
+        .to(img, time, { opacity: '0', onComplete: () => img.src = './assets/img/arr2/img1.jpg' }, '+=1')
+        .to(img, time, { opacity: '1' })
+        .to(img, time, { opacity: '0', onComplete: () => img.src = './assets/img/arr2/img8.jpg' }, '+=1')
+        .to(img, time, { opacity: '1' })
         .to(img, time, { opacity: '0', onComplete: () => img.src = './assets/img/arr2/img3.jpg' }, '+=1')
         .to(img, time, { opacity: '1' })
         .to(img, time, { opacity: '0', onComplete: () => img.src = './assets/img/arr2/img2.jpg' }, '+=1')
         .to(img, time, { opacity: '1' })
-        .to(img, time, { opacity: '0', onComplete: () => img.src = './assets/img/rose.png' })
-        .to(img, time, { opacity: '1' },'+=.3')
-        .to('.wish', 1, { opacity: '1', onComplete: () => document.querySelector('.filter').style.background ='rgba(0, 0, 0, 0.6)' })
+        .to(img, time, { opacity: '0', onComplete: () => img.src = './assets/img/rose.png' },'+=1')
+        .to(img, time, { opacity: '1' }, '+=.3')
+        .to('.wish', 1, { opacity: '1', onComplete: () => document.querySelector('.filter').style.background = 'rgba(0, 0, 0, 0.6)' })
         .to('.thankyou', 1, { opacity: '1' }, '-=1');
-        
+
 
 }
